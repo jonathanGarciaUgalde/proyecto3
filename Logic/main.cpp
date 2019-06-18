6c68bafdf646021e7e2982c2d326da2869994565
jonathanGarcia <jonagaru1@gmail.com>
2019-06-17 21:18:06 -0600

    cambio de posición de diagramas

---
+++ Logic/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jonathanGarcía on 10/06/19.
+//
+#include "GenericLinkedList.h"
+int main () {
+
+    GenericLinkedList<int> *genericLinkedList= new GenericLinkedList<int>;
+
+    genericLinkedList->add(23);
+
+
+    }
+
+
+
+
+
+
